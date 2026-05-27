80c133ff29e86a07fb33ce68f5824f92c6a2efb2
John Solomon <jaywisefreeworld59@gmail.com>
2026-05-27 18:15:04 +0100

    create function models for organization detail

--- src/router.js
+++ src/router.js
@@ -6,12 +6,16 @@ import { showProjectsPage } from './controllers/projects.js';
 import { showCategoriesPage } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
 
+import { showOrganizationDetailsPage } from './controllers/organizations.js';
+
+
 const router = express.Router();
 
 router.get('/', showHomePage);
 router.get('/organizations', showOrganizationsPage);
 router.get('/projects', showProjectsPage);
 router.get('/categories', showCategoriesPage);
+router.get('/organization/:id', showOrganizationDetailsPage);
 
 // error-handling routes
 router.get('/test-error', testErrorPage);
